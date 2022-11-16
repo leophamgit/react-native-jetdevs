e95eb48adb21f68a565092ea051358686a08616c
Leo Pham <leo.phamno1@outlook.com>
2022-11-17 00:12:05 +0700

    feat: setup vector icons

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 function LoginScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Login Screen</Text>
+      <Icon name="rocket" size={30} color="#900" />
     </View>
   );
 }
